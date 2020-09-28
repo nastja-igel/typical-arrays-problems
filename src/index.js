86c30ec431b93579e3fe0b33e08e3db31cde5326
Ilina <anastasia.ilina@t-systems.ru>
2020-09-28 22:43:27 +0300

    fix: add check for emty array or array w/o params

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 
 exports.min = function min (array) {
-  var min = Math.min(...array);
-  return min;
-}
+  if (Array.isArray(array)) {
+    if (array.length>0) {
+      var min = Math.min(...array);
+      return min;
+    };
+  }
+   
+  return 0;}
 
 exports.max = function max (array) {
-  var max = Math.max(...array);
-  return max;
-}
+  if (Array.isArray(array)) {
+    if (array.length>0) {
+      var max = Math.max(...array);
+      return max;
+    };
+  }
+   
+return 0;}
 
 exports.avg = function avg (array) {
-  var average = array.reduce((a, x) => a + x, 0) / array.length;
-  return average;
-}
+  if (Array.isArray(array)) {
+    if (array.length>0) {
+      var average = array.reduce((a, x) => a + x, 0) / array.length;
+      return average;
+    };
+  }
+   
+return 0;}
